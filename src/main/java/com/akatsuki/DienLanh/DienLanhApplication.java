3e7dd161f744d597eca37aaea83954af8e32a0b2
b1805859 <ngokinh30311@gmail.com>
2023-10-18 21:21:08 +0700

    project initial

---
+++ src/main/java/com/akatsuki/DienLanh/DienLanhApplication.java
@@ -0,0 +1,13 @@
+package com.akatsuki.DienLanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DienLanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DienLanhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akatsuki/DienLanh/DienLanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akatsuki.DienLanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DienLanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
